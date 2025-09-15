a1dcb19583da2e597eafd4710f5fe7c3028a4ed5
Jeet <jetdeula@gmail.com>
2025-09-15 21:31:41 +0545

    init : blog ts

---
+++ data/blog-mock.ts
@@ -0,0 +1,40 @@
+import { blogType } from "@/types/blog";
+
+export const mockblogs: blogType[] = [
+  {
+    id: 1,
+    title: "The Rise of AI in Everyday Life",
+    slug: "rise-of-ai-everyday-life",
+    author: "Jane Doe",
+    date: "2025-06-01",
+    tags: ["AI", "Machine Learning", "Technology Trends"],
+    summary:
+      "Artificial Intelligence is no longer just a buzzword. Here's how it's changing our daily routines.",
+    image:
+      "https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "Top 10 JavaScript Frameworks in 2025",
+    slug: "top-js-frameworks-2025",
+    author: "John Smith",
+    date: "2025-05-28",
+    tags: ["JavaScript", "Web Development", "Frameworks"],
+    summary:
+      "Discover the most popular JavaScript frameworks developers are using this year.",
+    image:
+      "https://images.unsplash.com/photo-1593376892929-d34f636e659e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "Why Edge Computing Is the Future",
+    slug: "edge-computing-future",
+    author: "Sara Lee",
+    date: "2025-05-15",
+    tags: ["Edge Computing", "IoT", "Cloud"],
+    summary:
+      "Edge computing is shifting how data is processed. Learn why it's gaining momentum.",
+    image:
+      "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
---
+++ types/blog.ts
@@ -0,0 +1,10 @@
+export type blogType = {
+  id: number;
+  title: string;
+  slug: string;
+  author: string;
+  date: string;
+  tags: string[];
+  summary: string;
+  image: string;
+};
