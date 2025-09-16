d64b91399f0de44e405097df51bdd197e49a58f2
Jeet <jetdeula@gmail.com>
2025-09-16 21:42:01 +0545

    updated types

--- constants/index.ts
+++ constants/index.ts
@@ -4,3 +4,29 @@ export const APP_DESCRIPTION =
   "Modern ecommerce store built with nextjs";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
+
+
+  export const navitems = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Shop",
+      href: "/shop",
+    },
+    {
+      name: "About",
+      href: "/about",
+    },
+    {
+      name: "Blogs",
+      href: "/blogs",
+    },
+    {
+      name: "Contact",
+      href: "/contact",
+    },
+    
+  ]
