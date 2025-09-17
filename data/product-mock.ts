11babf689d5f5ea970cf2e95c5edcae7bcf24fae
Jeet <jetdeula@gmail.com>
2025-09-17 10:07:48 +0545

    changed mock data styles

--- data/product-mock.ts
+++ data/product-mock.ts
@@ -3,16 +3,16 @@ import { productType } from "@/types/product";
 export const ProductMock: productType[] = [
   {
     id: "cmfja5gmi0223jw7x87udtwfb",
-    name: "Cosrx Hyaluronic Acid Intensive Cream",
-    slug: "cosrx-hyaluronic-acid-intensive-cream",
+    name: "Apple MacBook Pro 14-inch (M3, 2025)",
+    slug: "apple-macbook-pro-14-inch-m3-2025",
     description:
       '<p><span style="background-color: rgb(255, 255, 255); color: rgb(23, 23, 23);">Intensely hydrating cream boosts moisture content in your skin and seals it inside, protecting your skin from further loss of hydration. Without feeling oily or heavy, this intensively hydrating cream makes the skin feel soft like clouds and provides long-lasting moisture.</span></p><p><br></p>',
     featureImage:
-      "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/bf89d7a7-7529-4878-8da5-3b962f250623.jpg",
+      "https://images.unsplash.com/photo-1541807084-5c52b6b3adef?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 3000,
     stock: 4,
     offeredPrice: 2500,
-    isOfferedPriceActive: false,
+    isOfferedPriceActive: true,
     offerStartDate: null,
     offerEndDate: null,
     isActive: true,
@@ -29,45 +29,36 @@ export const ProductMock: productType[] = [
         productId: "cmfja5gmi0223jw7x87udtwfb",
         mediaType: "IMAGE",
         mediaUrl:
-          "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/5168eb2a-9548-4702-b123-6422cf42835f.jpg",
+          "https://images.unsplash.com/photo-1511385278-5432b00777ed?q=80&w=1180&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
     ],
     tags: [
-      {
-        id: "cmfja5gmj0225jw7xoknfwkc4",
-        name: "COSRX",
-      },
-      {
-        id: "cmfja5gmj0226jw7xg2b11w1l",
-        name: "Intensive Cream.",
-      },
-      {
-        id: "cmfja5gmj0227jw7xvutfiej6",
-        name: "CREAM",
-      },
+      { id: "t1", name: "Apple" },
+      { id: "t2", name: "Laptop" },
+      { id: "t3", name: "MacBook Pro" },
     ],
     seoMeta: {
       id: "08de0b00-2829-4377-8244-56db33888408",
       model: "Product",
-      metaTitle: "Cosrx Hyaluronic Acid Intensive Cream",
-      metaDescription: "BEST CREAM IN NEPAL",
-      metaKeywords: "COSRX, Intensive Cream, CREAM",
+      metaTitle: "Apple MacBook Pro 14-inch M3",
+      metaDescription: "Powerful 14-inch MacBook Pro with M3 chip in Nepal",
+      metaKeywords: "Apple, MacBook Pro, Laptop",
       metaRobots: null,
       createdAt: "2025-09-14T05:54:13.480Z",
       updatedAt: "2025-09-14T05:54:13.480Z",
     },
     brand: {
       id: "cmebcc4x7001ljw6vrfaq1b7d",
-      name: "Cosrx",
+      name: "Apple",
       image:
-        "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/7da0238c-6ffe-46ea-8775-c57a56de5d30.webp",
+        "https://images.unsplash.com/photo-1511385278-5432b00777ed?q=80&w=1180&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     reviews: [],
 
     category: {
       id: "cmemd01oi001kjweejy3jhlpo",
-      name: "SunScreen",
-      slug: "sunscreen-1",
+      name: "Laptops",
+      slug: "laptops",
       image: null,
       parentId: "cmeb7s3660001jw6vz3rwepch",
       isActive: true,
@@ -82,12 +73,12 @@ export const ProductMock: productType[] = [
   },
   {
     id: "cmfj9e4kz021njw7xjuw400j4",
-    name: "Cosrx Aloe Soothing Sun Cream SPF50+/ PA+++",
-    slug: "cosrx-aloe-soothing-sun-cream-spf50-pa",
+    name: "Logitech MX Master 3S Wireless Mouse",
+    slug: "logitech-mx-master-3s-wireless-mouse",
     description:
       '<p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">BENEFIT</span></p><p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">• Block UV rays </span></p><p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">• Soothes &amp; moisturizes skin </span></p><p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">• Lightweight texture</span></p><p><br></p><p><u style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">TARGETS</u></p><p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">• Doesn\'t want a white cast</span></p><p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">• Enjoy outdoor activities often</span></p><p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);">• Dehydrated skin</span></p><p><span style="background-color: rgba(232, 232, 232, 0.4); color: rgb(23, 23, 23);"> </span></p><p><br></p>',
     featureImage:
-      "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/1a69c7bf-a5ab-47a1-a61b-b0896ad772ed.jpg",
+      "https://images.unsplash.com/photo-1628832307345-7404b47f1751?q=80&w=1183&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 5000,
     stock: 4,
 
@@ -109,36 +100,26 @@ export const ProductMock: productType[] = [
         productId: "cmfj9e4kz021njw7xjuw400j4",
         mediaType: "IMAGE",
         mediaUrl:
-          "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/335e79be-a024-4fd8-8893-808e491fd688.jpg",
+          "https://images.unsplash.com/photo-1636489891987-c4eb210e23d2?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
     ],
     tags: [
-      {
-        id: "cmfj9e4l0021pjw7xdrpc955e",
-        name: "COSRX",
-      },
-      {
-        id: "cmfj9e4l0021qjw7xezpw7guj",
-        name: "SUNSCREAM",
-      },
-      {
-        id: "cmfj9e4l0021rjw7xc4cp1cty",
-        name: "ALOE SOOTHING  SUNCREAM",
-      },
+      { id: "t4", name: "Logitech" },
+      { id: "t5", name: "Wireless Mouse" },
     ],
     seoMeta: {
       id: "962786e5-a262-4b4c-becc-bd3b101b4bba",
       model: "Product",
-      metaTitle: "Cosrx Aloe Soothing Sun Cream SPF50+/ PA+++",
-      metaDescription: "BEST SOOTHING SUNCREAM IN NEPAL",
-      metaKeywords: "COCOSRX, SUNSCREAM, ALOE SOOTHING SUNCREAM",
+      metaTitle: "Logitech MX Master 3S Mouse",
+      metaDescription: "Best ergonomic wireless mouse in Nepal",
+      metaKeywords: "Logitech, Mouse, Wireless Mouse",
       metaRobots: null,
       createdAt: "2025-09-14T05:32:58.159Z",
       updatedAt: "2025-09-14T05:32:58.159Z",
     },
     brand: {
       id: "cmebcc4x7001ljw6vrfaq1b7d",
-      name: "Cosrx",
+      name: "Logitech",
       image:
         "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/7da0238c-6ffe-46ea-8775-c57a56de5d30.webp",
     },
@@ -146,8 +127,8 @@ export const ProductMock: productType[] = [
 
     category: {
       id: "cmemd01oi001kjweejy3jhlpo",
-      name: "SunScreen",
-      slug: "sunscreen-1",
+      name: "Mice",
+      slug: "mice",
       image: null,
       parentId: "cmeb7s3660001jw6vz3rwepch",
       isActive: true,
@@ -162,12 +143,12 @@ export const ProductMock: productType[] = [
   },
   {
     id: "cmfhsbprm01qijw7xo35jg8vq",
-    name: "Pax Moly 15% Niacinamide + 5% Zinc Serum 30ml",
-    slug: "pax-moly-15-niacinamide-5-zinc-serum-30ml",
+    name: "Razer BlackWidow V4 Pro Mechanical Keyboard",
+    slug: "razer-blackwidow-v4-pro-mechanical-keyboard",
     description:
       '<p><strong style="color: rgb(33, 37, 41);">The Pax Moly Trouble Relief Niacinamide 15% + Zinc 5% Serum is a potent serum designed to target blemishes, control oil production, and improve overall skin tone. With a high concentration of Niacinamide (15%) and Zinc (5%), this serum helps to calm acne-prone skin, reduce redness, and minimize the appearance of pores. The combination of these two key ingredients makes it ideal for those with oily, acne-prone, or troubled skin.</strong></p>',
     featureImage:
-      "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/ae111867-2e51-4f58-bf09-5f082da3ae16.png",
+      "https://images.unsplash.com/photo-1602025882379-e01cf08baa51?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 800,
     stock: 4,
 
@@ -189,40 +170,26 @@ export const ProductMock: productType[] = [
         productId: "cmfhsbprm01qijw7xo35jg8vq",
         mediaType: "IMAGE",
         mediaUrl:
-          "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/89954cdb-25bb-4f35-a7e2-4a51ea8e130c.jpeg",
+          "https://images.unsplash.com/photo-1742021856770-223f18e9f500?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
     ],
     tags: [
-      {
-        id: "cmfhsbprm01qkjw7xt8d0sodx",
-        name: "Pax Moly",
-      },
-      {
-        id: "cmfhsbprm01qljw7x4t5n2aba",
-        name: "15% Naicinamide",
-      },
-      {
-        id: "cmfhsbprm01qmjw7x79mlx5j8",
-        name: "5% Zinc",
-      },
-      {
-        id: "cmfhsbprm01qnjw7xo0po53lp",
-        name: "Face Serum",
-      },
+      { id: "t6", name: "Razer" },
+      { id: "t7", name: "Mechanical Keyboard" },
     ],
     seoMeta: {
       id: "7e633f48-4191-4757-98ae-310c44cbdf65",
       model: "Product",
-      metaTitle: "Pax Moly 15% Niacinamide + 5% Zinc Serum 30ml",
-      metaDescription: "Best Affordable Serum for Acne, Pores and Oily Skin",
-      metaKeywords: "Pax Moly, 15% Niacinamide, 5% Zinc, Face Serum",
+      metaTitle: "Razer BlackWidow V4 Pro",
+      metaDescription: "Premium mechanical gaming keyboard in Nepal",
+      metaKeywords: "Razer, Keyboard, Gaming",
       metaRobots: null,
       createdAt: "2025-09-13T04:47:25.998Z",
       updatedAt: "2025-09-13T04:47:25.998Z",
     },
     brand: {
       id: "cmebcbg0l001jjw6vb3hi7udl",
-      name: "Pax Moly",
+      name: "Razer",
       image:
         "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/601d03ee-4cd5-4c8c-a461-3bdc757c3145.png",
     },
@@ -230,8 +197,8 @@ export const ProductMock: productType[] = [
 
     category: {
       id: "cmemcw6y9001hjweeacp73noc",
-      name: "Face Serum",
-      slug: "face-serum",
+      name: "Keyboards",
+      slug: "keyboards",
       image: null,
       parentId: "cmeb7s3660001jw6vz3rwepch",
       isActive: true,
@@ -246,12 +213,12 @@ export const ProductMock: productType[] = [
   },
   {
     id: "cmfhqwj6q01phjw7xtrbgyt7x",
-    name: "L.A. Colors Radiant Foundation",
-    slug: "l-a-colors-radiant-foundation",
+    name: "SteelSeries Arctis Nova Pro Wireless Headset",
+    slug: "steelseries-arctis-nova-pro-wireless-headset",
     description:
       "<p>Perfect your complexion and achieve a radiant looking glow. Smooth, lightweight formula allows for flawless, buildable coverage without feeling heavy or weighed down.</p><p><br></p><p>Moisturizing ingredients with vitamin E and glycerin create a dewy finish and leaves the skin looking fresh all day.</p>",
     featureImage:
-      "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/17366cf9-567f-4388-92f9-0346f1a4f8d4.jpg",
+      "https://images.unsplash.com/photo-1673669236244-60f764c15f27?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 1800,
     stock: 4,
 
@@ -273,23 +240,26 @@ export const ProductMock: productType[] = [
         productId: "cmfhqwj6q01phjw7xtrbgyt7x",
         mediaType: "IMAGE",
         mediaUrl:
-          "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/0c468d3d-481f-4aa2-8a40-2c6c3025a8c7.webp",
+          "https://images.unsplash.com/photo-1612858249937-1cc0852093dd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
     ],
-    tags: [],
+    tags: [
+      { id: "t8", name: "SteelSeries" },
+      { id: "t9", name: "Wireless Headset" },
+    ],
     seoMeta: {
       id: "60fb4df1-918f-4dbd-91d2-91743e1fe888",
       model: "Product",
-      metaTitle: "L.A. Colors Radiant Foundation",
-      metaDescription: "Best radiant foundation from L.A.Colour in nepal",
-      metaKeywords: "L.A.Colour, Foundation, Radiant foundation",
+      metaTitle: "SteelSeries Arctis Nova Pro Wireless",
+      metaDescription: "High-end wireless gaming headset in Nepal",
+      metaKeywords: "SteelSeries, Head",
       metaRobots: null,
       createdAt: "2025-09-13T04:07:38.016Z",
       updatedAt: "2025-09-13T04:27:18.649Z",
     },
     brand: {
       id: "cmf0sgori000hjwywvzvyjekq",
-      name: "L.A. COLORS",
+      name: "SteelSeries",
       image:
         "https://lacosmeticsnepal.s3.ap-south-1.amazonaws.com/uploads/flag/cf68e348-b846-40aa-8e5f-0f34deaa049f.png",
     },
@@ -297,8 +267,8 @@ export const ProductMock: productType[] = [
 
     category: {
       id: "cmemckv53000ujweerv34mjl1",
-      name: "Foundations",
-      slug: "foundations",
+      name: "Headsets",
+      slug: "Headsets",
       image: null,
       parentId: "cmemcjxno000sjwee44x5rtw5",
       isActive: true,
