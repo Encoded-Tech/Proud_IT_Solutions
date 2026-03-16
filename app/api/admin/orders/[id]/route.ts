287cb109d7389312527f85946f53894ffb7fc37f
Sanjeev-Adhikari <sanjeevadhikari75@gmail.com>
2026-03-16 08:52:11 +0545

    added small get handler

--- app/api/admin/orders/[id]/route.ts
+++ app/api/admin/orders/[id]/route.ts
@@ -1,3 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({ message: "Temporary route disabled" });
+}
+
+
+
 // import { NextRequest, NextResponse } from "next/server";
 // import { withDB } from "@/lib/HOF";
 // import { withAuth } from "@/lib/HOF/withAuth";
